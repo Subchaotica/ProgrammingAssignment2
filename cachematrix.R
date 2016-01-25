74d817ee62ee61b6b34236fb77717bc0a555bdb4
datumdatum <ergodian@gmail.com>
2016-01-24 23:39:18 -0500

    Functions for inverting a matrix
    These functions complement each other to invert a matrix with persistent
    caching of functions and inversion results to minimize redundant
    calculations.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following complementary functions set up a matrix for inversion, using cached storage capabilities to 
+## avoid redundant calculations and providing persistence in the various stages of the process.
 
-## Write a short comment describing this function
+## The following function transforms a matrix into a special list that binds it with methods for setting up the matrix,
+## fetching it once it has been set up, storing its inverse, and fetching its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- matrix()
+  setmatrix <- function(y) {
+    x <<- y
+    m <<- matrix()
+  }
+  getmatrix <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(setmatrix = setmatrix, getmatrix = getmatrix,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
+  
 
 
-## Write a short comment describing this function
+
+## The following function invokes functions in makeCacheMatrix to check if the inverse has
+## already been calculated, and if so, fetching it from cached storage. If not, it calculates the inverse and 
+## sets it in cache.
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.na(m)) {
+    message("getting cached inverse")
+    return(m)
+  }
+  initmatrix <- x$getmatrix()
+  m <- solve(initmatrix)
+  x$setinverse(m)
+  m
 }
